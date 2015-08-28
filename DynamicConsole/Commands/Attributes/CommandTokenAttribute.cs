02339edd919cf3aa4b3dc201a32a34233e1804ad
jurajosef <josef.jura@hotmail.com>
2015-08-28 14:29:45 +0200

    0.8.3.0 - Added CommandTokenAttribute

---
+++ DynamicConsole/Commands/Attributes/CommandTokenAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicConsole.Commands.Attributes
+{
+    public class CommandTokenAttribute : CommandParameterAttribute
+    {
+        public CommandTokenAttribute(int index, string value)
+            : base("token", TypeCode.String)
+        {
+            this.Value = value;
+            this.Index = index;
+        }
+    }
+}
